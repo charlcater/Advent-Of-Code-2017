1f427932dd21e380c4dddab45379f7921d91a19c
Charl Cater <charl.cater@gmail.com>
2017-12-04 18:47:19 +0200

    day 2

---
+++ day_02/aoc_2017_02_1.py
@@ -0,0 +1,19 @@
+# Advent of Code 2017
+# Day 02 excercise 1
+
+import csv
+
+rowsum = []
+checksum = 0
+
+with open('input.txt', 'r') as f:
+    reader = csv.reader(f, delimiter='\t')
+    for row in reader:
+        rowlist = []
+        for value in (row):
+            rowlist.append(int(value))
+
+        diff = max(rowlist) - min(rowlist)
+        checksum += diff
+
+print('Checksum = {}'.format(checksum))
---
+++ day_02/aoc_2017_02_2.py
@@ -0,0 +1,33 @@
+# Advent of Code 2017
+# Day 02 excercise 2
+
+import csv
+
+rowsum = []
+checksum = 0
+
+with open('input.txt', 'r') as f:
+    reader = csv.reader(f, delimiter='\t')
+
+    for row in reader:
+        rowlist = []
+        for value in (row):
+            rowlist.append(int(value))
+        rowlist.sort(reverse=True)
+
+        length = len(rowlist)  # 16
+
+        i=0
+        b = True
+
+        for value in rowlist:
+            for i in range(len(rowlist)):
+                q = value / rowlist[i]
+                if (q).is_integer() and q != 1.0:
+                    checksum += q
+                    # print(q)
+                    # print('Cummalitave checksum = {}'.format(checksum))
+                else:
+                    pass
+
+print('Checksum = {}'.format(checksum))
